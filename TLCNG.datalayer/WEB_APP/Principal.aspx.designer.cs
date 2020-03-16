d15b0a9dae235f7fba67680466418fe401000110
pinachaimd <pinachaimd@hotmail.com>
2020-03-16 12:15:56 -0600

    capas logicas

--- TLCNG.datalayer/WEB_APP/Principal.aspx.designer.cs
+++ TLCNG.datalayer/WEB_APP/Principal.aspx.designer.cs
@@ -7,11 +7,13 @@
 // </generado automáticamente>
 //------------------------------------------------------------------------------
 
+using System.Web.UI.HtmlControls;
+
 namespace WEB_APP {
     
     
     public partial class Principal {
-        
+
         /// <summary>
         /// Control form1.
         /// </summary>
@@ -19,6 +21,8 @@ namespace WEB_APP {
         /// Campo generado automáticamente.
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        private global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        protected HtmlForm Form1 { get => form1; set => form1 = value; }
     }
 }
